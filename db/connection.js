1eaffc1e07bb05fcc10afc8b28d984240cc590df
Mohiuddin Tamim <mohiuhere@gmail.com>
2022-06-18 21:01:37 +0600

    db connected

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+
+const connectDB = function(url){
+    return mongoose.connect(url)
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 const app = express()
 const indexRouter = require('./router/index')
-
+const connectDB = require('./db/connection')
 
 
 app.use(bodyParser.json())
@@ -10,6 +11,21 @@ app.use(bodyParser.json())
 app.use('/', indexRouter)
 
 
-
 const port = process.env.port || 8080
-app.listen(port, console.log(`Server is running on ${port}...`))
+
+
+// Server only start whene db is connected to server
+const start = async function(){
+    try{
+        await connectDB(process.env.DATABASE_URL)
+        app.listen(port, console.log(`Server is running on ${port}...`))
+    }catch(err){
+        console.error(err)
+    }
+}
+
+start()
+
+// End==> Server only start whene db is connected to server
+
+
